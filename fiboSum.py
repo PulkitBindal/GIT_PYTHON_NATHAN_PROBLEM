c97055cb3bb030a0611d057e85f7c19e47773b43
Mudit Srivastava <60660510+muditbit@users.noreply.github.com>
2021-10-01 07:57:24 +0530

    Update fiboSum.py
    Sum Of N Terms of Fibonacci Series

--- fiboSum.py
+++ fiboSum.py
@@ -1,6 +1,29 @@
 def fiboSum(n):
-     #logic here
-      pass
+     #logic her
+     if N == 0:
+
+        return 0
+ 
+
+     
+
+     Sum = 0
+
+     a, b = 0, 1
+
+     Sum += a 
+     count += 1
+
+     while count <= N: 
+ 
+
+        Sum += a
+
+        a, b = b, a + b 
+
+     
+
+     return Sum
 
 Nterms= int(input())
 out_ = fiboSum(Nterms)
